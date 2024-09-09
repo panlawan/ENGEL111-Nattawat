e6c1e2972d8a2478be99da067cd8e7b6ba972df3
panlawan <nattawat_pa62@edu.rmutl.ac.th>
2024-09-09 10:05:09 +0700

    update 2024-09-09

--- src/module.h
+++ src/module.h
@@ -7,7 +7,7 @@ WiFi 3 Mode
 2 -> STA (Station)
 3 -> APSTA (Access Point and Station)
 */
-
+#define DEBUG
 
 
 /* ------------------ Start Lib ------------------- */
@@ -18,11 +18,39 @@ WiFi 3 Mode
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPmDNS.h>
-
+#include <map>
 /* ------------------- End Lib --------------------*/
 
 
 /* ------------------ Start Variable ------------------- */
+
+extern IPAddress ip;
+extern IPAddress gateways;
+extern IPAddress subnets;
+extern IPAddress dns;
+extern std::map<String, String> env_vars;
+
+extern String MODES;
+extern String AP_SSID;
+extern String AP_PSK;
+extern String STA_SSID;
+extern String STA_PSK;
+extern String mqtt_enable;
+extern String mqtt_broker;
+extern String mqtt_port;
+extern String mqtt_topic;
+extern String mqtt_username;
+extern String mqtt_password;
+extern String mqtt_interval;
+extern String RTU_enable;
+extern String RTU_deviceID;
+extern String RTU_baudRate;
+extern String TCP_wifi_enable;
+extern String TCP_eth_enable;
+extern String local_IP;
+extern String gateway;
+extern String subnet;
+extern String dnss;
 
 extern AsyncWebServer server;
 
@@ -35,6 +63,12 @@ extern const char* password;
 
 
 /* ------------------ Start Function ------------------- */
+void writeEnvFile();
+void readEnvFile();
+void updateEnvVariable(const String &key, const String &value);
+bool configureNetwork(String IPAddressStr, String gatewayStr, String subnetStr, String dnsa);
+IPAddress stringToIPAddress(String ipStr);
+
 void setup_wifi();
 void setup_wifiAP();
 
